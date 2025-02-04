cb5901025e4cd2366947c290b101d5f6e49172b0
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-02-03 22:55:50 -0300

    initial commit

---
+++ ApiTokenBearer/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ApiTokenBearer.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    [HttpGet("login")]
+    public IActionResult Login(string username,string password)
+    {
+        if(IsValidUser(username, password))
+        {
+            var claimsPrincipal = new ClaimsPrincipal
+                (
+                    new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, username) },
+                    BearerTokenDefaults.AuthenticationScheme
+                ));
+
+            return SignIn(claimsPrincipal);
+        }
+
+        return Unauthorized();  
+    }
+
+
+    [HttpGet("/user")]
+    public IActionResult GetUser()
+    {
+        var user = User;
+        if (user?.Identity?.IsAuthenticated?? false)
+        {
+            return Ok($"Bem vindo{user.Identity.Name}");
+        }
+        
+        return Unauthorized();
+    }
+
+    private bool IsValidUser(string username, string password)
+    {
+        return username == "rickccastro" && password == "123456";
+    }
+}
---
+++ ApiTokenBearer/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "apicatalogo", Version = "v1" });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "Token",
+        In = ParameterLocation.Header,
+        Description = "Token Access",
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+builder.Services.AddAuthentication().AddBearerToken();
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
